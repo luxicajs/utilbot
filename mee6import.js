8a6e86dc99be194a8c8d224e22630cef6d7d0f53
Atsui <79206024+atsuijsx@users.noreply.github.com>
2023-04-16 22:03:08 +0200

    Create mee6import.js

---
+++ mee6import.js
@@ -0,0 +1,17 @@
+import fetch from "node-fetch";
+import { QuickDB } from "quick.db";
+
+const db = new QuickDB({ filePath: "./levels.db" });
+
+async function importPages(num) {
+    for (let i = 0; i < num; i++) {
+        const dataFetch = await fetch(`https://mee6.xyz/api/plugins/levels/leaderboard/168406162302173184?page=${i}`);
+        const data = await dataFetch.json();
+        for await (const player of data.players) {
+            await db.set(player.id, { xp: player.detailed_xp[0], allXp: player.xp, level: player.level });
+            console.log(`Imported for ${player.username}#${player.discriminator}`);
+        }
+    }
+}
+
+importPages(15);

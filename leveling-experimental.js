71b254e7bfaec610ec47c1c054c5c88d7e273146
Atsui <79206024+atsuijsx@users.noreply.github.com>
2023-06-16 00:11:41 +0200

    Leveling overhaul
    Optimization and much, much cleaner code.

--- leveling-experimental.js
+++ leveling-experimental.js
@@ -5,7 +5,7 @@ import {
   EmbedBuilder,
   AttachmentBuilder,
 } from "discord.js";
-import { QuickDB } from "quick.db";
+import { GreatDB, Schema, DataType } from "great.db";
 import canvacord from "canvacord";
 import fs from "node:fs";
 import isNumber from "is-number";
@@ -21,17 +21,29 @@ const bot = new Client({
   ],
 });
 
-const db = new QuickDB({ filePath: "./levels.db" });
+const db = new GreatDB.Database({
+  type: GreatDB.Type.Disk,
+  name: "levels",
+});
 
-let recentChatters = new Set();
+const userSchema = Schema.Create({
+  id: DataType.String,
+  level: DataType.Number,
+  xp: DataType.Number,
+  allXp: DataType.BigInt,
+});
+
+const table = db.table("levels", userSchema);
+
+const recentChatters = new Set();
 const prefix = ".";
+
+bot.on("ready", async () => {
+  console.log(`Bot ready as ${bot.user.username}`);
+});
 
 const minXp = 15;
 const maxXp = 25;
-
-bot.on("ready", async () => {
-    console.log("Bot ready");
-});
 
 function xpNeeded(lvl) {
   let xp = 100;
@@ -49,26 +61,33 @@ bot.on("messageCreate", async (message) => {
 
   if (!recentChatters.has(authorId)) {
     const xpToAdd = Math.floor(Math.random() * (maxXp - minXp + 1)) + minXp;
-    const user = await db.get(authorId);
 
-    if (user) {
-      await db.set(`${authorId}.xp`, user.xp + xpToAdd);
-      await db.set(`${authorId}.allXp`, user.allXp + xpToAdd);
+    const userExists = await table.has("id", authorId); // Is this a joke?
+
+    if (userExists) {
+      const user = await table.get("id", authorId);
+      await table.set({
+        id: authorId,
+        xp: user.xp + xpToAdd,
+        allXp: user.allXp + xpToAdd,
+      });
       if (user.xp + xpToAdd > xpNeeded(user.level)) {
-        await db.set(authorId, {
-          xp: user.xp + xpToAdd - xpNeeded(user.level),
+        const setXp = user.xp + xpToAdd - xpNeeded(user.level);
+        await table.set({
+          id: authorId,
+          xp: setXp,
           level: user.level + 1,
           allXp: user.allXp + xpToAdd,
         });
       }
     } else {
-      await db.set(authorId, { level: 0, xp: xpToAdd, allXp: xpToAdd });
+      await table.set({ id: authorId, level: 0, xp: xpToAdd, allXp: xpToAdd });
     }
 
     recentChatters.add(authorId);
 
     setTimeout(() => {
-      delete recentChatters[authorId];
+      recentChatters.delete(authorId);
     }, 1000 * 60);
   }
 
@@ -79,74 +98,87 @@ bot.on("messageCreate", async (message) => {
 
     switch (command) {
       case "leaderboard":
-        let page = isNumber(args[0]) ? Number(args[0]) - 1 : 0;
-        const all = await db.all();
-        const numberOfPages = Math.ceil(all.length / 10);
-        if (page >= numberOfPages) {
-          page = numberOfPages - 1;
-        }
-        const sorted = all
-          .sort((a, b) => b["value"]["allXp"] - a["value"]["allXp"])
-          .slice(page * 10, page * 10 + 10);
-        const placement = all.findIndex((e) => e.id == message.author.id) + 1;
+      case "lb":
+      case "ld":
+        const lbLength = await db.executeQuery(
+          `SELECT count(id) as lbLength FROM levels;`
+        );
 
-        let lbString = "";
+        const rank = await db.executeQuery(`SELECT COUNT(*) + 1 AS rank
+                FROM levels
+                WHERE allXp > (SELECT allXp FROM levels WHERE id = '${message.author.id}');`);
 
-        sorted.forEach((e, i) => {
-          const did = message.guild.members.cache.get(e.id);
-          const username = did
-            ? `${did.user.username}#${did.user.discriminator}`
-            : `<@${e.id}>`;
-          lbString += `**${i + 1 + page * 10}**. ${username} - Level ${
-            e.value.level
-          } | ${e.value.xp.toLocaleString()} XP\n`;
-        });
+        const page = Math.min(
+          args[0] && isNumber(args[0]) ? args[0] - 1 : 0,
+          Math.ceil(lbLength[0].lbLength / 10) - 1
+        );
+
+        const q1 = await db.executeQuery(
+          `SELECT * FROM levels ORDER BY allXp DESC LIMIT 10 OFFSET ${
+            page * 10
+          }`
+        );
 
         const lbEmbed = new EmbedBuilder()
           .setTitle(`Leaderboard Page ${page + 1}`)
-          .setDescription(lbString)
+          .setDescription(
+            q1
+              .map((e, i) => {
+                return `${i + 1 + page * 10}. ${
+                  message.guild.members.cache.get(e.id) || `<@${e.id}>`
+                } - Level ${e.level} | ${e.xp.toLocaleString()} XP`;
+              })
+              .join("\n")
+          )
           .setFooter({
-            text: `Page ${
-              page + 1
-            } of ${numberOfPages} | Your rank: ${placement}/${all.length}`,
+            text: `Page ${page + 1} of ${
+              Math.ceil(lbLength[0].lbLength / 10) - 1
+            } | Your rank: ${rank[0]["rank"]}/${lbLength[0].lbLength}`,
           });
 
         message.channel.send({ embeds: [lbEmbed] });
         break;
+
       case "rank":
-        if (args[0])
-          return message.channel.send(
-            "You can't view someone's rank yet... It's still a work in progress."
-          );
-        const player = await db.get(authorId);
-        const dat = await db.all();
-        const rank =
-          dat
-            .sort((a, b) => b["value"]["allXp"] - a["value"]["allXp"])
-            .findIndex((e) => e.id == message.author.id) + 1;
+      case "r":
+        const rankUser = message.guild.members.cache.get(
+          args[0] && isNumber(args[0]) ? args[0] : message.author.id
+        ).user;
+
+        const userInfo = await table.get("id", rankUser.id);
+
+        if (!userInfo) return message.reply("User not found.");
+
+        const placement = await db.executeQuery(`SELECT COUNT(*) + 1 AS rank
+                FROM levels
+                WHERE allXp > (SELECT allXp FROM levels WHERE id = '${rankUser.id}');`);
 
         const card = new canvacord.Rank()
           .setAvatar(
-            message.author.displayAvatarURL({
+            rankUser.displayAvatarURL({
               format: "png",
               size: 256,
             })
           )
           .setOverlay("transparent")
-          .setCurrentXP(player.xp)
-          .setLevel(player.level)
-          .setRequiredXP(xpNeeded(player.level), "#AAA")
+          .setCurrentXP(userInfo.xp)
+          .setLevel(userInfo.level)
+          .setRequiredXP(xpNeeded(userInfo.level), "#AAA")
           .setProgressBar("#0074D9")
           .setBackground("IMAGE", fs.readFileSync("./final.png"))
-          .setUsername(message.author.username)
-          .setDiscriminator(message.author.discriminator)
-          .setRank(rank);
+          .setUsername(rankUser.username)
+          .setRank(placement[0].rank);
         card.build().then((data) => {
           const attachment = new AttachmentBuilder(Buffer.from(data), {
             name: "unknown.png",
           });
           message.channel.send({ files: [attachment] });
         });
+
+        break;
+
+      case "help":
+        message.reply("`.leaderboard/.ld/.lb` - Leaderboard | `.rank/.r` - Rank");
         break;
     }
   }
